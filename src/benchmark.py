28d2c4708ce707c44cf9171e8f4ad52df8ee4da5
alexaleluia12 <alexdiasaleluia@gmail.com>
2022-11-08 11:06:31 -0300

    add count hit

--- src/benchmark.py
+++ src/benchmark.py
@@ -9,6 +9,13 @@ def countAccert(actual, guess):
 
     return len(sActual.intersection(sGuess)) == 6
 
+def countAmountAccerts(actual, guess):
+    sActual = set(actual)
+    sGuess = set(guess)
+
+    return len(sActual.intersection(sGuess))
+
+
 def verifyTotalHits(lines, generatorType):
     generator = generatorType()
     ns = rg.SequenceNumber()
@@ -20,23 +27,58 @@ def verifyTotalHits(lines, generatorType):
             hits += 1
     return hits
 
-with open('./msnumbers', 'rb') as file:
-    lines = pickle.load(file)
-    lines = convert_to_list(lines)
+def verifyGroupsOfHits(lines, generatorType):
+    generator = generatorType()
+    ns = rg.SequenceNumber()
 
-    print('total registers', len(lines))
+    hits = {}
+    for line in lines:
+        guess = ns.create(generator)
+        count = countAmountAccerts(line, guess)
+        if count in hits:
+            hits[count] += 1
+        else:
+            hits[count] = 1
 
-    hitsBF = verifyTotalHits(lines, rg.GeneratorBFLow)
-    print('hits for BF', hitsBF)
+    return hits
 
-    hitsAA = verifyTotalHits(lines, rg.GeneratorAALow)
-    print('hits for aa', hitsAA)
+def allWin():
+    with open('./msnumbers', 'rb') as file:
+        lines = pickle.load(file)
+        lines = convert_to_list(lines)
 
-    hitsPR = verifyTotalHits(lines, rg.GeneratorPureRandom)
-    print('hits for pr', hitsPR)
+        print('total registers', len(lines))
+
+        hitsBF = verifyTotalHits(lines, rg.GeneratorBFLow)
+        print('hits for BF', hitsBF)
+
+        hitsAA = verifyTotalHits(lines, rg.GeneratorAALow)
+        print('hits for aa', hitsAA)
+
+        hitsPR = verifyTotalHits(lines, rg.GeneratorPureRandom)
+        print('hits for pr', hitsPR)
+
+def hits():
+    print('hits\n\n')
+    with open('./msnumbers', 'rb') as file:
+        lines = pickle.load(file)
+        lines = convert_to_list(lines)
+
+        print('total registers', len(lines))
+
+        hitsBF = verifyGroupsOfHits(lines, rg.GeneratorBFLow)
+        print('hits for BF', hitsBF)
+
+        hitsAA = verifyGroupsOfHits(lines, rg.GeneratorAALow)
+        print('hits for aa', hitsAA)
+
+        hitsPR = verifyGroupsOfHits(lines, rg.GeneratorPureRandom)
+        print('hits for pr', hitsPR)
+
+hits()
 
 """
-current results:
+current results (allWin):
 total registers 2514
 hits for BF 0
 hits for aa 0
