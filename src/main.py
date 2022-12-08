87013c9dee51a180dcd1c9da9265c2079a8865b5
alexaleluia12 <alexdiasaleluia@gmail.com>
2022-12-08 10:06:00 -0300

    add api method to generate random numbers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
+
+from enum import Enum
+
+from fastapi import FastAPI
+
 import randon_generator as rg
 
-gen = rg.GeneratorPureRandom()
-seq = rg.SequenceNumber()
-s = seq.create(gen)
-print(s)
+
+class ModelNumbers(str, Enum):
+    modelOne = "modelOne"
+    modelTwo = "modelTwo"
+    modelThree = "modelThree"
+
+app = FastAPI()
+
+
+@app.get("/numbers/{model}")
+async def createNumber(model: ModelNumbers):
+    mapNumbesGenerators = {
+        "modelOne": rg.GeneratorPureRandom,
+        "modelTwo": rg.GeneratorBFLow,
+        "modelThree": rg.GeneratorAALow,
+    }
+    gen = mapNumbesGenerators[model.value]
+    seq = rg.SequenceNumber()
+    return seq.create(gen())
